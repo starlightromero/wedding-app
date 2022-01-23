6a0220495001e5ffb68254562ef62b4c4125092d
Starlight Romero <starlightromero@protonmail.com>
2022-01-23 14:41:33 -0800

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM golang:1.17.6-alpine as build
+
+RUN apk add ca-certificates=20191127-r5 --no-cache
+
+WORKDIR /app
+
+COPY go.mod .
+COPY go.sum .
+
+RUN go mod download
+
+COPY . .
+
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build .
+
+FROM scratch
+
+COPY --from=build /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
+COPY --from=build /app/m /usr/bin/m
+
+ENTRYPOINT ["/usr/bin/m"]
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func main() {
+	t := &Template{
+		templates: template.Must(template.ParseGlob("public/views/*.html")),
+	}
+
+	e := echo.New()
+	e.Renderer = t
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.POST},
+	}))
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.File("/", "public/views/home.html")
+	e.POST("/", postRSVP)
+	e.GET("/health", getHealth)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func getHealth(c echo.Context) error {
+	return c.JSON(http.StatusOK, "healthy")
+}
+
+func postRSVP(c echo.Context) error {
+	name := c.FormValue("name")
+	dietaryRestrictions := c.FormValue("dietaryRestrictions")
+	attending := c.FormValue("attending")
+	return c.String(http.StatusOK, "Thank you for RSVPing! name: "+name+"dietary restrictions:  "+dietaryRestrictions+"attending: "+attending)
+}
